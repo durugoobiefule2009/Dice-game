518d636adb80f2b1ebc4c3bcbcd79e860cb21120
Obiefule Ikechukwu Durugo <durugoobiefule2009@gmail.com>
2026-05-17 12:44:52 +0100

    Add files via upload

---
+++ Dice game/index.js
@@ -0,0 +1,15 @@
+function Play() {
+    let randomNumber1 = Math.floor(Math.random() * 6) + 1;
+    document.getElementsByClassName("img1")[0].setAttribute("src", './images/dice' + randomNumber1 + '.png');
+
+    let randomNumber2 = Math.floor(Math.random() * 6) + 1;
+    document.getElementsByClassName("img2")[0].setAttribute("src", './images/dice' + randomNumber2 + '.png');
+
+    if (randomNumber1 > randomNumber2) {
+        document.querySelector("h1").innerHTML = "Player 1 Wins! 🚩";
+    } else if (randomNumber2 > randomNumber1) {
+        document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+    } else {
+    document.querySelector("h1").innerHTML = "It's a Draw! 🤝";
+}
+}
